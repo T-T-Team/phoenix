b4f0262168b045b272c565a803c8d7d39da58281
Toma <novotny.tom96@gmail.com>
2026-07-25 13:15:58 +0200

    adjusted timings

--- common/src/main/java/dev/tnt/phoenix/data/game/BetMultiplier.java
+++ common/src/main/java/dev/tnt/phoenix/data/game/BetMultiplier.java
@@ -6,12 +6,12 @@ import net.minecraft.util.StringRepresentable;
 public enum BetMultiplier implements StringRepresentable {
 
     X1("x1", 1, 0),
-    X2("x2", 2, 20),
-    X5("x5", 5, 50),
-    X10("x10", 10, 120),
-    X25("x25", 25, 200),
-    X50("x50", 50, 240),
-    X100("x100", 100, 360);
+    X2("x2", 2, 10),
+    X5("x5", 5, 20),
+    X10("x10", 10, 30),
+    X25("x25", 25, 40),
+    X50("x50", 50, 50),
+    X100("x100", 100, 60);
 
     public static final Codec<BetMultiplier> CODEC = StringRepresentable.fromEnum(BetMultiplier::values);
     private final String serializedName;
